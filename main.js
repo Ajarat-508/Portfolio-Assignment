b116d474cb0a443faf63b397cecea6404e455d1d
Ajarat Azeez <ajaratazeez508@gmail.com>
2023-08-02 17:32:19 +0100

    updated the javasript codes, smooth scroll and hamburger menu

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-$('#navbar-sticky, a, .btn').on('click', function(event) {
-  if (this.hash !== '') {
-    event.preventDefault();
-
-    const hash = this.hash;
-
-    $('html, body').animate(
-      {
-        scrollTop: $(hash).offset().top -100
-      },
-      800
-    );
-  }
+$('a').click(function (e) {
+  $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top -100
+  }, 1000);
 });
+
+let hamburger = document.getElementById("hamburger");
+let mobileNav = document.getElementById("mobile-nav");
+
+hamburger.addEventListener("click", function() {
+  mobileNav.classList.toggle("hidden");
+});
+
